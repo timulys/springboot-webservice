ee10e643b9199cc45b13b6725466fbbe6b68a00c
최종현 <timulys@gmail.com>
2020-09-26 17:21:22 +0900

    springboot-webservice init

---
+++ src/main/java/com/timulys/webservice/Application.java
@@ -0,0 +1,13 @@
+package com.timulys.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
---
+++ src/main/java/com/timulys/webservice/web/WebRestController.java
@@ -0,0 +1,13 @@
+package com.timulys.webservice.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WebRestController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "helloworld!";
+    }
+}
