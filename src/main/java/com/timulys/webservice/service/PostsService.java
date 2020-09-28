f39bf3d1dfd9986cbaf904896ec54cbdbbd6d059
최종현 <timulys@gmail.com>
2020-09-28 13:53:06 +0900

    commit

--- src/main/java/com/timulys/webservice/service/PostsService.java
+++ src/main/java/com/timulys/webservice/service/PostsService.java
@@ -7,7 +7,6 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
