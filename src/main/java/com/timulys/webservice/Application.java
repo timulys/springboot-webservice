cafd0870590c536dc308fafda87066c94de94604
최종현 <timulys@gmail.com>
2020-10-06 12:52:54 +0900

    무중단 배포 설정 추가

--- src/main/java/com/timulys/webservice/Application.java
+++ src/main/java/com/timulys/webservice/Application.java
@@ -2,14 +2,21 @@ package com.timulys.webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing // Jpa Auditing 활성화
 @SpringBootApplication
 public class Application {
 
+    public static final String APPLICATION_LOCATIONS = "spring.config.localtion=" +
+            "classpath:application.yml," +
+            "/app/config/springboot-webservice/real-application.yml";
+
     public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
+        new SpringApplicationBuilder(Application.class)
+                .properties(APPLICATION_LOCATIONS)
+                .run(args);
     }
 
 }
--- src/main/java/com/timulys/webservice/web/WebRestController.java
+++ src/main/java/com/timulys/webservice/web/WebRestController.java
@@ -3,16 +3,27 @@ package com.timulys.webservice.web;
 import com.timulys.webservice.dto.posts.PostsSaveRequestDto;
 import com.timulys.webservice.service.PostsService;
 import lombok.AllArgsConstructor;
+import org.springframework.core.env.Environment;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
 
 @RestController
 @AllArgsConstructor
 public class WebRestController {
 
     private PostsService postsService;
+    private Environment env;
+
+    @GetMapping("/profile")
+    public String getProfile() {
+        return Arrays.stream(env.getActiveProfiles())
+                .findFirst()
+                .orElse("");
+    }
 
     @GetMapping("/hello")
     public String hello() {
