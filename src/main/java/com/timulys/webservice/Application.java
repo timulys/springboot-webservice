a2d5c0efe1de038821c3b1378c2262f2ff909407
최종현 <timulys@gmail.com>
2020-10-08 12:35:29 +0900

    7. nginx를 통한 무중단 배포 마무리

--- src/main/java/com/timulys/webservice/Application.java
+++ src/main/java/com/timulys/webservice/Application.java
@@ -1,6 +1,5 @@
 package com.timulys.webservice;
 
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
